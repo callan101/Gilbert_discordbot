54124e66fed3ec436f0ce4ac2ba9d04faa16b034
Callan <me@callan101.com>
2023-10-24 09:18:31 -0700

    js bot functional

---
+++ bot.js
@@ -0,0 +1,76 @@
+
+
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+
+const readline = require('readline');
+
+
+const commands = [{
+  name: 'goodmorning',
+  description: 'Sends a good morning message!',
+  },
+  {
+  name: 'ping',
+  description: 'ping pong',
+  }];
+
+const rest = new REST({ version: '9' }).setToken('NjU3MTAyMTA2MDA3OTYxNjIw.GpTcBX.SXV8uWcCbtdwsHLlRwEO9L8ZSrg4gPvKyxkmyM');
+
+(async () => {
+  try {
+    console.log('Started refreshing application (/) commands.');
+
+    await rest.put(
+      Routes.applicationGuildCommands('657102106007961620', '798767830823600128'),
+      { body: commands },
+    );
+
+    console.log('Successfully reloaded application (/) commands.');
+  } catch (error) {
+    console.error(error);
+  }
+})();
+
+const { Client, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages] });
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}`);
+  console.log(`WebSocket Ping: ${client.ws.ping}ms`);
+});
+
+client.on('interactionCreate', async (interaction) => {
+  if (!interaction.isCommand()) return;
+
+  const { commandName } = interaction;
+
+  if (commandName === 'goodmorning') {
+    await interaction.reply('Good morning!');
+  }
+
+  if (commandName === 'ping') {
+    console.log('pong ' + client.ws.ping);
+
+    await interaction.reply('pong ' + client.ws.ping); // client.ws.ping gives the bot's latency
+  }
+});
+
+client.login('NjU3MTAyMTA2MDA3OTYxNjIw.GpTcBX.SXV8uWcCbtdwsHLlRwEO9L8ZSrg4gPvKyxkmyM');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.question('Bot is running. Type "STOP" to quit: ', (answer) => {
+  if (answer.toUpperCase() === 'STOP') {
+    console.log('Stopping the bot...');
+    client.destroy();
+    rl.close();
+    process.exit(0);
+  } else {
+    console.log(`Invalid command: ${answer}`);
+    rl.close();
+  }
+});
